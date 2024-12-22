71989d6e8cbec303f0810952d3ff6140262904b5
Şerif Çam <serifcam34@gmail.com>
2024-12-22 17:19:50 +0300

    Güncellemeler eklendi

--- lib/screens/group_detail_screen.dart
+++ lib/screens/group_detail_screen.dart
@@ -16,18 +16,15 @@ class _GroupDetailScreenState extends State<GroupDetailScreen> {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  String? _selectedUserId; // Seçilen kullanıcının ID'si
-  String? _groupCreatorId; // Grubu oluşturan kişinin ID'si
-  String? _currentUserId; // Mevcut kullanıcının ID'si
+  String? _selectedUserId;
+  String? _groupCreatorId;
 
   @override
   void initState() {
     super.initState();
     _fetchGroupCreator();
-    _currentUserId = _auth.currentUser?.uid;
   }
 
-  // Grubun kurucusunu çekme
   Future<void> _fetchGroupCreator() async {
     final groupDoc =
         await _firestore.collection('groups').doc(widget.groupId).get();
@@ -38,7 +35,44 @@ class _GroupDetailScreenState extends State<GroupDetailScreen> {
     }
   }
 
-  // UID'ye karşılık gelen e-posta adresini çekmek için fonksiyon
+  Future<void> _toggleDebtStatus() async {
+    if (_selectedUserId == null) return;
+
+    try {
+      final querySnapshot = await _firestore
+          .collection('debts')
+          .where('groupId', isEqualTo: widget.groupId)
+          .where('fromUser', isEqualTo: _selectedUserId)
+          .limit(1)
+          .get();
+
+      if (querySnapshot.docs.isNotEmpty) {
+        final doc = querySnapshot.docs.first;
+        final currentStatus = doc['status'];
+        final newStatus = currentStatus == 'paid' ? 'pending' : 'paid';
+
+        await _firestore.collection('debts').doc(doc.id).update({
+          'status': newStatus,
+        });
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Borç durumu güncellendi!')),
+        );
+
+        setState(() {
+          _selectedUserId = null;
+        });
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Seçilen kullanıcı için borç bulunamadı.')),
+        );
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('Hata: $e')));
+    }
+  }
+
   Future<String> _getUserEmail(String uid) async {
     final userDoc = await _firestore.collection('users').doc(uid).get();
     if (userDoc.exists && userDoc.data() != null) {
@@ -48,45 +82,61 @@ class _GroupDetailScreenState extends State<GroupDetailScreen> {
     }
   }
 
-  // Borç durumunu güncelleme fonksiyonu
-  Future<void> _toggleDebtStatus() async {
-    if (_selectedUserId == null) return;
-
+  Future<void> _deleteGroup() async {
     try {
-      final querySnapshot = await _firestore
+      await _firestore.collection('groups').doc(widget.groupId).delete();
+      final debtsSnapshot = await _firestore
           .collection('debts')
           .where('groupId', isEqualTo: widget.groupId)
-          .where('fromUser', isEqualTo: _selectedUserId)
           .get();
 
-      for (var doc in querySnapshot.docs) {
-        final currentStatus = doc['status'];
-        final newStatus = currentStatus == 'paid' ? 'pending' : 'paid';
-
-        await _firestore.collection('debts').doc(doc.id).update({
-          'status': newStatus,
-        });
+      for (var debtDoc in debtsSnapshot.docs) {
+        await _firestore.collection('debts').doc(debtDoc.id).delete();
       }
 
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Borç durumu güncellendi!')),
+        SnackBar(content: Text('Grup başarıyla silindi!')),
       );
 
-      setState(() {
-        _selectedUserId = null; // Seçimi sıfırla
-      });
+      Navigator.of(context).pop();
     } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Hata: $e')),
-      );
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('Hata: $e')));
+    }
+  }
+
+  Future<void> _showDeleteConfirmationDialog() async {
+    final confirm = await showDialog<bool>(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Text('Onay'),
+        content: Text(
+            'Herkes borcunu ödemiştir, grubu silmek istediğinize emin misiniz?'),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(ctx).pop(false),
+            child: Text('Hayır'),
+          ),
+          TextButton(
+            onPressed: () => Navigator.of(ctx).pop(true),
+            child: Text('Evet'),
+          ),
+        ],
+      ),
+    );
+
+    if (confirm == true) {
+      await _deleteGroup();
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    final currentUserId = _auth.currentUser?.uid;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('${widget.groupName} '),
+        title: Text('${widget.groupName} Detayları'),
       ),
       body: StreamBuilder<QuerySnapshot>(
         stream: _firestore
@@ -94,8 +144,9 @@ class _GroupDetailScreenState extends State<GroupDetailScreen> {
             .where('groupId', isEqualTo: widget.groupId)
             .snapshots(),
         builder: (context, snapshot) {
-          if (!snapshot.hasData)
+          if (!snapshot.hasData) {
             return Center(child: CircularProgressIndicator());
+          }
 
           final debts = snapshot.data!.docs;
 
@@ -121,8 +172,8 @@ class _GroupDetailScreenState extends State<GroupDetailScreen> {
                   itemCount: debts.length,
                   itemBuilder: (context, index) {
                     final debt = debts[index].data() as Map<String, dynamic>;
-                    bool isCreator = debt['fromUser'] == debt['toUser'];
-                    bool isSelected = _selectedUserId == debt['fromUser'];
+                    final isCreator = debt['fromUser'] == _groupCreatorId;
+                    final isSelected = _selectedUserId == debt['fromUser'];
 
                     return FutureBuilder<String>(
                       future: _getUserEmail(debt['fromUser']),
@@ -145,60 +196,91 @@ class _GroupDetailScreenState extends State<GroupDetailScreen> {
                               fontWeight: isCreator
                                   ? FontWeight.bold
                                   : FontWeight.normal,
+                              color: isCreator ? Colors.amber : null,
                             ),
                           ),
                           subtitle: isCreator
                               ? Text(
-                                  'Tüm harcamayı ödedi',
+                                  'Tüm hesabı ödemiştir',
                                   style: TextStyle(
-                                      fontSize: 12, color: Colors.grey),
+                                    fontSize: 12,
+                                    color: Colors.grey,
+                                  ),
                                 )
                               : Text('Borç: ${debt['amount']} TL'),
-                          trailing: Text(
-                            isCreator
-                                ? 'Kurucu'
-                                : debt['status'] == 'pending'
-                                    ? 'Borçlu'
-                                    : 'Ödendi',
-                            style: TextStyle(
-                              color: isCreator
-                                  ? Colors.amber
-                                  : debt['status'] == 'pending'
-                                      ? Colors.red
-                                      : Colors.green,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          tileColor: isSelected
-                              ? Colors.teal.withOpacity(0.2)
-                              : null, // Seçilen kullanıcıyı vurgula
-                          onTap: () {
-                            if (_groupCreatorId == _currentUserId) {
-                              setState(() {
-                                _selectedUserId = debt[
-                                    'fromUser']; // Kullanıcı seçimini kaydet
-                              });
-                            }
-                          },
+                          trailing: isCreator
+                              ? Icon(
+                                  Icons.star,
+                                  color: Colors.amber,
+                                )
+                              : Text(
+                                  debt['status'] == 'pending'
+                                      ? 'Borçlu'
+                                      : 'Ödendi',
+                                  style: TextStyle(
+                                    color: debt['status'] == 'pending'
+                                        ? Colors.red
+                                        : Colors.green,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                          tileColor:
+                              isSelected ? Colors.teal.withOpacity(0.2) : null,
+                          onTap: isCreator
+                              ? null
+                              : () {
+                                  setState(() {
+                                    _selectedUserId = debt['fromUser'];
+                                  });
+                                },
                         );
                       },
                     );
                   },
                 ),
               ),
+              // Bu butonlar yalnızca grup kurucusuna görünecek
+              if (currentUserId == _groupCreatorId)
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0, vertical: 16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      OutlinedButton.icon(
+                        onPressed: _showDeleteConfirmationDialog,
+                        icon: Icon(Icons.delete, color: Colors.red),
+                        label: Text(
+                          'Grubu Sil',
+                          style: TextStyle(color: Colors.red),
+                        ),
+                        style: OutlinedButton.styleFrom(
+                          side: BorderSide(color: Colors.red),
+                          padding: EdgeInsets.symmetric(
+                              vertical: 12.0, horizontal: 20.0),
+                        ),
+                      ),
+                      OutlinedButton.icon(
+                        onPressed:
+                            _selectedUserId != null ? _toggleDebtStatus : null,
+                        icon: Icon(Icons.check, color: Colors.green),
+                        label: Text(
+                          'Ödendi',
+                          style: TextStyle(color: Colors.green),
+                        ),
+                        style: OutlinedButton.styleFrom(
+                          side: BorderSide(color: Colors.green),
+                          padding: EdgeInsets.symmetric(
+                              vertical: 12.0, horizontal: 20.0),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
             ],
           );
         },
       ),
-      floatingActionButton: _groupCreatorId == _currentUserId
-          ? FloatingActionButton.extended(
-              onPressed: _selectedUserId != null ? _toggleDebtStatus : null,
-              label: Text('Ödedi'),
-              icon: Icon(Icons.check),
-              backgroundColor:
-                  _selectedUserId != null ? Colors.green : Colors.grey,
-            )
-          : null, // Buton sadece grup kurucusuna gösterilecek
     );
   }
 }
